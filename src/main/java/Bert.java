094faf85cd2d56173bc9379d7d99756afb99073a
gbinw128 <gbinw128@gmail.com>
2025-08-29 15:07:09 +0800

    Level-0: Rename Duke to Bert, added Greet and Exit messages

---
+++ src/main/java/Bert.java
@@ -0,0 +1,9 @@
+public class Bert {
+    public static void main(String[] args) {
+        String welcomeMessage = " Hello! I'm BERT - Bot for Echo, Response and Talk\n" +
+                " What can I do for you?";
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(welcomeMessage);
+        System.out.println(goodbyeMessage);
+    }
+}
