2aba96f9ffb241835fd86b4e6adeeead1b99a48f
gbinw128 <gbinw128@gmail.com>
2025-08-29 15:35:16 +0800

    Level-1:Added Echo function

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Bert {
     public static void main(String[] args) {
         String welcomeMessage = " Hello! I'm BERT - Bot for Echo, Response and Talk\n" +
                 " What can I do for you?";
         String goodbyeMessage = "Bye. Hope to see you again soon!";
         System.out.println(welcomeMessage);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while(!line.contentEquals("bye"))
+        {
+            System.out.println(line);
+            line = in.nextLine();
+        }
+
         System.out.println(goodbyeMessage);
     }
 }
