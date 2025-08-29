b100f151b31f706e037bb68bf3e23cc6b79ee237
gbinw128 <gbinw128@gmail.com>
2025-08-29 15:50:25 +0800

    Level-2:Added the ability to save text and listing it, added indentation for Bert's Text

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -2,18 +2,33 @@ import java.util.Scanner;
 
 public class Bert {
     public static void main(String[] args) {
-        String welcomeMessage = " Hello! I'm BERT - Bot for Echo, Response and Talk\n" +
-                " What can I do for you?";
-        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        String welcomeMessage = "\t Hello! I'm BERT - Bot for Echo, Response and Talk\n" +
+                "\t What can I do for you?";
+        String goodbyeMessage = "\t Bye. Hope to see you again soon!";
         System.out.println(welcomeMessage);
 
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
+        String[] list = new String[100];
+        int listIndex = 0;
+
         while(!line.contentEquals("bye"))
         {
-            System.out.println(line);
+            if(line.contentEquals("list"))
+            {
+                for(int i = 0; i < listIndex; i++)
+                {
+                    System.out.println("\t"+i+". " + list[i]);
+                }
+            }
+            else
+            {
+                list[listIndex] = line;
+                listIndex++;
+                System.out.println("\tadded " + line);
+            }
             line = in.nextLine();
         }
 
