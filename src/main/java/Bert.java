08e22ba28adbd6e3c979478318a0afc4a58637d6
gbinw128 <gbinw128@gmail.com>
2025-09-04 11:28:04 +0800

    Added unmarkTask, markTask and listTasks function in Bert.java

--- src/main/java/Bert.java
+++ src/main/java/Bert.java
@@ -1,35 +1,51 @@
 import java.util.Scanner;
 
 public class Bert {
+    private static void unmarkTask(String line, Task[] tasks) {
+        String[] markNumber = line.split(" ");
+        int taskNumToUnmark = Integer.parseInt(markNumber[1]) - 1;
+        tasks[taskNumToUnmark].unmarkAsDone();
+        System.out.println("\tOk, I've unmarked this task:");
+        System.out.println("\t" + tasks[taskNumToUnmark]);
+    }
+
+    private static void markTask(String line, Task[] tasks) {
+        String[] markNumber = line.split(" ");
+        int taskNumToMark = Integer.parseInt(markNumber[1]) - 1;
+        tasks[taskNumToMark].markAsDone();
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t" + tasks[taskNumToMark]);
+    }
+
+    private static void listTasks(int taskIndex, Task[] tasks) {
+        System.out.println("\tHere are the tasks in your list:");
+
+        for(int i = 1; i < taskIndex + 1; ++i) {
+            System.out.println("\t" + i + ". " + tasks[i - 1]);
+        }
+
+    }
+
     public static void main(String[] args) {
-        String welcomeMessage = "\t Hello! I'm BERT - Bot for Echo, Response and Talk\n" +
-                "\t What can I do for you?";
+        String welcomeMessage = "\t Hello! I'm BERT - Bot for Echo, Response and Talk\n\t What can I do for you?";
         String goodbyeMessage = "\t Bye. Hope to see you again soon!";
         System.out.println(welcomeMessage);
-
-        String line;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
+        String line = in.nextLine();
+        Task[] tasks = new Task[100];
 
-        String[] list = new String[100];
-        int listIndex = 0;
-
-        while(!line.contentEquals("bye"))
-        {
-            if(line.contentEquals("list"))
-            {
-                for(int i = 0; i < listIndex; i++)
-                {
-                    System.out.println("\t"+i+". " + list[i]);
-                }
-            }
-            else
-            {
-                list[listIndex] = line;
-                listIndex++;
+        for(int taskIndex = 0; !line.contentEquals("bye"); line = in.nextLine()) {
+            if (line.contentEquals("list")) {
+                listTasks(taskIndex, tasks);
+            } else if (line.startsWith("mark")) {
+                markTask(line, tasks);
+            } else if (line.startsWith("unmark")) {
+                unmarkTask(line, tasks);
+            } else {
+                tasks[taskIndex] = new Task(line);
+                ++taskIndex;
                 System.out.println("\tadded " + line);
             }
-            line = in.nextLine();
         }
 
         System.out.println(goodbyeMessage);
